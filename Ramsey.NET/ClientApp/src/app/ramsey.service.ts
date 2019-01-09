eba8da25e7b7985a956849d619e7e345aa9d39f1
joel linder <Jollescott.carl@hotmail.com>
2019-01-09 13:54:25 +0100

    updated client with new api

--- Ramsey.NET/ClientApp/src/app/ramsey.service.ts
+++ Ramsey.NET/ClientApp/src/app/ramsey.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core'
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class RamseyService {
@@ -9,13 +8,13 @@ export class RamseyService {
   }
 
   async getRecipes(ingredients: Ingredient[]): Promise<Recipe[]> {
-    const url = window.location.origin + '/recipe/suggest';
+    const url = window.location.origin + '/v2/recipe/suggest';
 
     return await this.http.post<Recipe[]>(url, ingredients).toPromise();
   }
 
   async getIngredients(search: string): Promise<Ingredient[]> {
-    const url = window.location.origin + '/ingredient/suggest?search=' + search;
+    const url = window.location.origin + '/v2/ingredient/suggest?search=' + search;
     return await this.http.get<Ingredient[]>(url).toPromise();
   }
 }
@@ -30,11 +29,19 @@ export interface Recipe {
 
 export interface Ingredient {
   ingredientId: string;
+  role: IngredientRole;
   recipeParts: RecipePart[];
+}
+
+export enum IngredientRole {
+  Include, Exclude 
 }
 
 export interface RecipePart {
   ingredientId: string;
   recipeId: string;
+
+  unit: string;
+  quantity: number;
 }
 
