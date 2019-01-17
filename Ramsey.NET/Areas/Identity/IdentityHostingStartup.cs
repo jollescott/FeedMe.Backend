b36b2c6456fad50b191ba75b587f6f8a1999a3e8
joel linder <Jollescott.carl@hotmail.com>
2019-01-17 18:50:01 +0100

    work on admin panel

---
+++ Ramsey.NET/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Ramsey.NET.Areas.Identity.Data;
+using Ramsey.NET.Models;
+
+[assembly: HostingStartup(typeof(Ramsey.NET.Areas.Identity.IdentityHostingStartup))]
+namespace Ramsey.NET.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<AdminContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("AdminContextConnection")));
+
+                services.AddDefaultIdentity<RamseyAdmin>()
+                    .AddEntityFrameworkStores<AdminContext>();
+            });
+        }
+    }
+}
