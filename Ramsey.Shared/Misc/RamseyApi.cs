2d0b99eedf93b2b0da2a2b7b3524314e0ee0a440
joel linder <Jollescott.carl@hotmail.com>
2019-02-11 22:20:11 +0100

    Added apis

--- Ramsey.Shared/Misc/RamseyApi.cs
+++ Ramsey.Shared/Misc/RamseyApi.cs
@@ -35,6 +35,12 @@ namespace Ramsey.Shared.Misc
                 public static string Retreive => Base + "v2/recipe/retrieve";
                 public static string Text => Base + "v2/recipe/text";
             }
+
+            public class Tags
+            {
+                public static string Suggest => Base + "v2/tags/suggest";
+                public static string List => Base + "v2/tags/list";
+            }
         }
     }
 }
