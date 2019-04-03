f3eb0fdb82512a2b5d676aaf8e63a9e0c4f46502
joel linder <Jollescott.carl@hotmail.com>
2019-04-03 14:43:40 +0200

    Added meta controller and get recipe count api

---
+++ Ramsey.NET/Controllers/Interfaces/V2/IMetaController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Ramsey.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ramsey.NET.Controllers.Interfaces.V2
+{
+    public interface IMetaController
+    {
+        IActionResult GetRecipeCount(RamseyLocale locale = RamseyLocale.Swedish);
+    }
+}
