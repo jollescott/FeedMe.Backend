0801a0a48e233e619b305ee16073a0e82c8f9ee4
joel linder <Jollescott.carl@hotmail.com>
2019-05-28 17:43:02 +0200

    Bumped to 2.2

--- Ramsey.NET/Startup.cs
+++ Ramsey.NET/Startup.cs
@@ -34,7 +34,7 @@ namespace Ramsey.NET
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.AddMvc().AddJsonOptions(options => {
                 options.SerializerSettings.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
