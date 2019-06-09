cf07007dd9d1bdaaf1e401417781d4484d9f2d01
joel linder <Jollescott.carl@hotmail.com>
2019-06-10 01:07:31 +0200

    Updated tests

---
+++ Ramsey.NET.Tests/Tests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Ramsey.NET.Tests
+{
+    public class Tests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
