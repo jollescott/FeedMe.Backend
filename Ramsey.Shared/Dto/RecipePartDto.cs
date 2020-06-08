f5bedde0bcb282e9bed5489d8b1c330f739a2605
Joel Linder <jollescott.carl@hotmail.com>
2020-06-07 18:21:22 -0700

    Removed Ramsey.Shared

--- Ramsey.Shared/Dto/RecipePartDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Ramsey.Shared.Dto
-{
-    public class RecipePartDto
-    {
-        public int IngredientId { get; set; }
-        public string RecipeId { get; set; }
-    }
-}
--- Ramsey.Shared/Dto/V2/TagDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Ramsey.Shared.Dto.V2
-{
-    public class TagDto
-    {
-        public int TagId { get; set; }
-        public string Name { get; set; }
-        public string PreviewImage { get; set; }
-    }
-}
