f54fb6886307956f50f3b21d784a56f8ca2011dc
Joel Linder <joelsc.linder@gmail.com>
2023-02-04 16:38:55 +0100

    Removed unused packages

--- Ramsey.NET/Startup.cs
+++ Ramsey.NET/Startup.cs
@@ -29,26 +29,19 @@ namespace Ramsey.NET
             Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
         }
 
-        public IConfiguration Configuration { get; }
+        private IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            /*
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
-
-            services.AddMvc().AddJsonOptions(options =>
-            {
-                options.JsonSerializerOptions.ReferenceLoopHandling = ReferenceLoopHandling.Ignore;
-            });
-            */
+            // TODO: Migrate endpoints.
+            services.AddMvc().AddMvcOptions(opt => opt.EnableEndpointRouting = false);
 
             // In production, the Angular files will be served from this directory
             services.AddSpaStaticFiles(configuration =>
             {
                 configuration.RootPath = "ClientApp/build";
             });
-
 
             var connectionString = Configuration.GetConnectionString(Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Production" ? "RamseyRelease" : "RamseyDebug");
             services.AddDbContext<IRamseyContext, RamseyContext>(options => options.UseNpgsql(connectionString));
@@ -101,21 +94,9 @@ namespace Ramsey.NET
             app.UseHangfireServer();
             app.UseHangfireDashboard();
 
-            if (!env.IsDevelopment())
-            {
-                RecurringJob.AddOrUpdate(() => serviceProvider.GetRequiredService<ICrawlerService>().StartIndexUpdate(), Cron.Weekly);
-                BackgroundJob.Enqueue(() => serviceProvider.GetRequiredService<ICrawlerService>().StartIndexUpdate());
-            }
-
-            app.UseSpa(spa =>
-            {
-                spa.Options.SourcePath = "ClientApp";
-
-                if (env.IsDevelopment())
-                {
-                    spa.UseReactDevelopmentServer(npmScript: "start");
-                }
-            });
+            if (env.IsDevelopment()) return;
+            RecurringJob.AddOrUpdate(() => serviceProvider.GetRequiredService<ICrawlerService>().StartIndexUpdate(), Cron.Weekly);
+            BackgroundJob.Enqueue(() => serviceProvider.GetRequiredService<ICrawlerService>().StartIndexUpdate());
         }
     }
 }
